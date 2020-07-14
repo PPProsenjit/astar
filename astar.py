1342f82949ab8ca6d10537db5625bad3ca61ef45
Max Belushkin <maxb@onehourtranslation.com>
2020-07-14 11:30:05 +0300

    Added astar algorithm

--- astar.py
+++ astar.py
@@ -57,7 +57,13 @@ class PriorityQueue:
         return heapq.heappop(self.elements)[1]
 
 
-def dijkstra_search(graph, start, goal):
+def heuristic(a, b):
+    (x1, y1) = a
+    (x2, y2) = b
+    return abs(x1 - x2) + abs(y1 - y2)
+
+
+def a_star_search(graph, start, goal):
     frontier = PriorityQueue()
     frontier.put(start, 0)
     came_from = {}
@@ -75,30 +81,17 @@ def dijkstra_search(graph, start, goal):
             new_cost = cost_so_far[current] + graph.cost(current, next)
             if next not in cost_so_far or new_cost < cost_so_far[next]:
                 cost_so_far[next] = new_cost
-                priority = new_cost
+                priority = new_cost + heuristic(goal, next)
                 frontier.put(next, priority)
                 came_from[next] = current
 
     return came_from, cost_so_far
 
 
-def reconstruct_path(came_from, start, goal):
-    current = goal
-    path = []
-    while current != start:
-        path.append(current)
-        current = came_from[current]
-    path.append(start) # optional
-    path.reverse() # optional
-    return path
-
-
 from implementation import *
-
-
-came_from, cost_so_far = dijkstra_search(diagram4, (1, 4), (7, 8))
-draw_grid(diagram4, width=3, point_to=came_from, start=(1, 4), goal=(7, 8))
+start, goal = (1, 4), (7, 8)
+came_from, cost_so_far = a_star_search(diagram4, start, goal)
+draw_grid(diagram4, width=3, point_to=came_from, start=start, goal=goal)
 print()
-draw_grid(diagram4, width=3, number=cost_so_far, start=(1, 4), goal=(7, 8))
+draw_grid(diagram4, width=3, number=cost_so_far, start=start, goal=goal)
 print()
-draw_grid(diagram4, width=3, path=reconstruct_path(came_from, start=(1, 4), goal=(7, 8)))
